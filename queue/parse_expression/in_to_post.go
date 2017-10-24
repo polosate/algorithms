1918890658c4d0905211188f76bb533d52b43079
Anastasiya Shaposhnikova <ana.shaposhnikova@lazada.com>
2017-10-24 19:49:24 +0300

    Infix to postfix

--- queue/parse_expression/in_to_post.go
+++ queue/parse_expression/in_to_post.go
@@ -5,7 +5,7 @@ import "fmt"
 type inToPost struct {
 	stack   *operatorsStack
 	inBytes []byte
-	outStr  string
+	outStr  []byte
 }
 
 func NewInToPost(inStr string) *inToPost {
@@ -13,25 +13,51 @@ func NewInToPost(inStr string) *inToPost {
 	return &inToPost{
 		stack:   NewOperandsStack(len(inputBytes)),
 		inBytes: inputBytes,
-		outStr:  "",
+		outStr:  []byte{},
 	}
 }
 
+// A + B × (C – D)
+// 1.
+
 func (this *inToPost) DoTrans() {
-	var strCh string
 	for _, v := range this.inBytes {
-		strCh = string(v)
-		switch strCh {
-		case "+", "-":
-			fmt.Println()
-		case "*", "/":
-			fmt.Println()
-		case "(":
-			fmt.Println()
-		case ")":
+		switch v {
+		case string("+"), string("-"):
+			this.gotOp(v, 1)
+		case string("*"), string("/"):
+			this.gotOp(v, 2)
+		case string("("):
+			this.stack.push(v)
+		case string(")"):
 			fmt.Println()
 		default:
-			this.outStr += strCh
+			this.outStr = append(this.outStr, v)
 		}
 	}
+}
+
+func (this *inToPost) gotOp(op byte, priority1 int) {
+	for !this.stack.isEmpty() {
+		stOp := this.stack.pop()
+		if stOp == byte("(") {
+			this.stack.push(stOp)
+			break
+		} else {
+			var priority2 int
+			switch stOp {
+			case byte("+"), byte("-"):
+				priority2 = 1
+			case byte("*"), byte("/"):
+				priority2 = 2
+			}
+			if priority2 < priority1 {
+				this.stack.push(stOp)
+				break
+			} else {
+				this.outStr = append(this.outStr, stOp)
+			}
+		}
+	}
+	this.stack.push(op)
 }
