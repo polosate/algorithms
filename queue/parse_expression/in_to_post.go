e4eb4ff02bcb26e058347572cef1e2b7e1de05a1
Anastasiya Shaposhnikova <ana.shaposhnikova@lazada.com>
2017-10-24 16:59:27 +0300

    DoTrans

--- queue/parse_expression/in_to_post.go
+++ queue/parse_expression/in_to_post.go
@@ -1,20 +1,37 @@
 package parse_expression
 
+import "fmt"
+
 type inToPost struct {
-	stack *operatorsStack
-	inBytes   []byte
+	stack   *operatorsStack
+	inBytes []byte
 	outStr  string
 }
 
 func NewInToPost(inStr string) *inToPost {
 	inputBytes := []byte(inStr)
 	return &inToPost{
-		stack:        NewOperandsStack(len(inputBytes)),
-		inBytes:   inputBytes,
-		outStr: "",
+		stack:   NewOperandsStack(len(inputBytes)),
+		inBytes: inputBytes,
+		outStr:  "",
 	}
 }
 
 func (this *inToPost) DoTrans() {
-	
+	var strCh string
+	for _, v := range this.inBytes {
+		strCh = string(v)
+		switch strCh {
+		case "+", "-":
+			fmt.Println()
+		case "*", "/":
+			fmt.Println()
+		case "(":
+			fmt.Println()
+		case ")":
+			fmt.Println()
+		default:
+			this.outStr += strCh
+		}
+	}
 }
