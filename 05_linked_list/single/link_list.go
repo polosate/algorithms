a2ba0eed11da741b569a9fa3476014bfa130e894
Anastasiya Shaposhnikova <ana.shaposhnikova@lazada.com>
2017-10-30 14:34:02 +0300

    Methods for single linked list

--- 05_linked_list/single/link_list.go
+++ 05_linked_list/single/link_list.go
@@ -1,11 +1,23 @@
 package single
 
+import "fmt"
+
+type ISingleLinkList interface {
+	Delete(key int) *Link
+	DeleteFirst() *Link
+	DisplayList()
+	Find(key int) *Link
+	InsertAfter(key, iData int, dData float32)
+	InsertFirst(iData int, dData float32)
+	IsEmpty() bool
+}
+
 type LinkList struct {
 	first *Link
 }
 
-func NewLinkList() LinkList {
-	return LinkList{
+func NewLinkList() ISingleLinkList {
+	return &LinkList{
 		first: nil,
 	}
 }
@@ -32,4 +44,38 @@ func (ll *LinkList) DisplayList() {
 		current.DisplayLink()
 		current = current.next
 	}
+	fmt.Println("--------")
+}
+
+func (ll *LinkList) Find(key int) *Link {
+	current := ll.first
+	for current != nil {
+		if current.iData == key {
+			return current
+		}
+		current = current.next
+	}
+	return current
+}
+
+func (ll *LinkList) Delete(key int) *Link {
+	previous := ll.first
+	current := ll.first
+
+	for current != nil {
+		if current.iData == key {
+			if current == ll.first {
+				ll.first = ll.first.next
+			} else {
+				previous.next = current.next
+			}
+			return current
+		}
+		previous = current
+		current = current.next
+	}
+	return nil
+}
+
+func (ll *LinkList) InsertAfter(key, iData int, dData float32) {
 }
--- 05_linked_list/single/link_list_test.go
+++ 05_linked_list/single/link_list_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestInsertDeleteFirst(t *testing.T) {
 	ll := NewLinkList()
 	if !ll.IsEmpty() {
 		t.Error("Expected IsEmpty", true, "have", false)
@@ -23,4 +23,76 @@ func Test(t *testing.T) {
 	if ll.IsEmpty() {
 		t.Error("Expected IsEmpty", false, "have", true)
 	}
+}
+
+func TestFind(t *testing.T) {
+	ll := NewLinkList()
+	el := ll.Find(3)
+	if el != nil {
+		t.Error("Expected next", nil, "have", el.next)
+	}
+
+	ll.InsertFirst(1, 1.1)
+	ll.InsertFirst(2, 2.2)
+	ll.InsertFirst(3, 3.3)
+	ll.InsertFirst(4, 4.4)
+	ll.InsertFirst(5, 5.5)
+
+	el = ll.Find(3)
+	if el.dData != 3.3 {
+		t.Error("Expected next", 3.3, "have", el.dData)
+	}
+
+	el = ll.Find(6)
+	if el != nil {
+		t.Error("Expected next", nil, "have", el)
+	}
+
+	el = ll.Find(1)
+	if el.dData != 1.1 {
+		t.Error("Expected next", 1.1, "have", el.dData)
+	}
+
+	el = ll.Find(5)
+	if el.dData != 5.5 {
+		t.Error("Expected next", 5.5, "have", el.dData)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	ll := NewLinkList()
+	el := ll.Delete(3)
+	if el != nil {
+		t.Error("Expected next", nil, "have", el.next)
+	}
+
+	ll.InsertFirst(1, 1.1)
+	ll.InsertFirst(2, 2.2)
+	ll.InsertFirst(3, 3.3)
+	ll.InsertFirst(4, 4.4)
+	ll.InsertFirst(5, 5.5)
+
+	el = ll.Delete(3)
+	if el.dData != 3.3 {
+		t.Error("Expected next", 3.3, "have", el.dData)
+	}
+	ll.DisplayList()
+
+	el = ll.Delete(6)
+	if el != nil {
+		t.Error("Expected next", nil, "have", el)
+	}
+	ll.DisplayList()
+
+	el = ll.Delete(1)
+	if el.dData != 1.1 {
+		t.Error("Expected next", 1.1, "have", el.dData)
+	}
+	ll.DisplayList()
+
+	el = ll.Delete(5)
+	if el.dData != 5.5 {
+		t.Error("Expected next", 5.5, "have", el.dData)
+	}
+	ll.DisplayList()
 }
