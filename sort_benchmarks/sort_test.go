3527a567da09010a8a460ac5ffc1e6ec6ec2b0c7
polosate <polosate@gmail.com>
2017-11-14 16:13:47 +0300

    Shell sort benchmark

--- sort_benchmarks/sort_test.go
+++ sort_benchmarks/sort_test.go
@@ -8,6 +8,7 @@ import (
 	insert "algorithms/02_compare_sort/inserting"
 	sel "algorithms/02_compare_sort/selecting"
 	recursion "algorithms/07_recursion"
+	shell "algorithms/shell_sort"
 )
 
 var (
@@ -15,26 +16,28 @@ var (
 	bubbleA bubble.BArray
 	insertA insert.IArray
 	selectA sel.SArray
+	shellA  shell.ShellArray
 )
 
 func TestMain(m *testing.M) {
 	res := []int64{}
-	for i := 0; i < 500000; i++ {
+	for i := 0; i < 500000	; i++ {
 		res = append(res, int64(rand.Int()))
 	}
 	mergeA = recursion.NewMArray(res)
 	bubbleA = bubble.NewBArray(res)
 	insertA = insert.NewIArray(res)
 	selectA = sel.NewSArray(res)
+	shellA = shell.NewShellArray(res)
 	m.Run()
+}
+
+func BenchmarkShellSort(b *testing.B) {
+	shellA.ShellSort()
 }
 
 func BenchmarkMergeSort(b *testing.B) {
 	mergeA.MergeSort()
-}
-
-func BenchmarkBubbleSort(b *testing.B) {
-	bubbleA.BubbleSort()
 }
 
 func BenchmarkInsertingSort(b *testing.B) {
@@ -43,4 +46,8 @@ func BenchmarkInsertingSort(b *testing.B) {
 
 func BenchmarkSelectionSort(b *testing.B) {
 	selectA.SelectingSort()
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	bubbleA.BubbleSort()
 }
