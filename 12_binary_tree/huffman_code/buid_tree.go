da115218a965343c9389ea597b9c9e6d53fb6f59
polosate <polosate@gmail.com>
2017-12-04 18:00:45 +0300

    Huffman code

--- 12_binary_tree/huffman_code/buid_tree.go
+++ 12_binary_tree/huffman_code/buid_tree.go
@@ -1,1 +1,42 @@
 package huffman_code
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Node struct {
+	value      string
+	leftChild  *Node
+	rightChild *Node
+}
+
+func NewNode(value string) *Node {
+	return &Node{value: value}
+}
+
+func (n *Node) GetValue() string {
+	return n.value
+}
+
+func (n *Node) DisplayNode() {
+	fmt.Print(n.value, "; ")
+}
+
+type Tree struct {
+	root *Node
+}
+
+func NewTree() Tree {
+	return Tree{root: nil}
+}
+
+type huffmanTree struct {
+	text         string
+	frequencyMap map[int]string
+	codeMap      map[byte]string
+}
+
+func NewHuffmanTree() huffmanTree {
+	return huffmanTree{}
+}
