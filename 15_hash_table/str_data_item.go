a2252117c15224001266b47dd92725456097240a
polosate <polosate@gmail.com>
2017-12-21 11:01:27 +0300

    Hash string

--- 15_hash_table/str_data_item.go
+++ 15_hash_table/str_data_item.go
@@ -1,1 +1,19 @@
 package _5_hash_table
+
+import "fmt"
+
+type StrDataItem struct {
+	key string
+}
+
+func NewStrDataItem(key string) *StrDataItem {
+	return &StrDataItem{key: key}
+}
+
+func (d StrDataItem) GetKey() string {
+	return d.key
+}
+
+func (d StrDataItem) DisplayDataItem() {
+	fmt.Print(d.key)
+}
