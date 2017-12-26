af46b1a7ca639d398b45d351c6e383c583a5d94e
ashaposhnikova <polosate@gmail.com>
2017-12-26 21:18:23 +0300

    Add reversed lists

--- 05_linked_list/exercises/add_lists.go
+++ 05_linked_list/exercises/add_lists.go
@@ -1,1 +1,41 @@
 package exercises
+
+func addLists(l1, l2 *singleList) *singleList {
+	f := addListsRec(l1.first, l2.first, 0)
+	res := NewSingleList()
+	res.first = f
+	return res
+}
+
+func addListsRec(l1, l2 *link, carry int64) *link {
+	if l1 == nil && l2 == nil && carry == 0 {
+		return nil
+	}
+	value := carry
+	if l1 != nil {
+		value += l1.value
+	}
+
+	if l2 != nil {
+		value += l2.value
+	}
+	res := NewLink(value % 10)
+	if l1 == nil {
+		l1 = nil
+	} else {
+		l1 = l1.next
+	}
+	if l2 == nil {
+		l2 = nil
+	} else {
+		l2 = l2.next
+	}
+	if value > 10 {
+		carry = 1
+	} else {
+		carry = 0
+	}
+	next := addListsRec(l1, l2, carry)
+	res.next = next
+	return res
+}
--- 05_linked_list/exercises/add_lists_test.go
+++ 05_linked_list/exercises/add_lists_test.go
@@ -1,1 +1,27 @@
 package exercises
+
+import "testing"
+
+func TestAddLists(t *testing.T) {
+	l1 := NewSingleList()
+	l1.InsertFirst(3)
+	l1.InsertFirst(2)
+	l1.InsertFirst(1)
+
+	l2 := NewSingleList()
+	l2.InsertFirst(9)
+	l2.InsertFirst(1)
+
+	res := addLists(l1, l2)
+
+	expected := []int64{2, 1, 4}
+	i := 0
+	cur := res.first
+	for cur != nil {
+		if cur.value != expected[i] {
+			t.Error("Want", expected[i], "have", cur.value)
+		}
+		i++
+		cur = cur.next
+	}
+}
