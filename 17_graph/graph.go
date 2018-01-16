644617b85e913fc13b0fbd21f5b429d26e09dcfe
polosate <polosate@gmail.com>
2018-01-16 13:34:59 +0300

    Depth-first search

--- 17_graph/graph.go
+++ 17_graph/graph.go
@@ -1,6 +1,10 @@
 package _7_graph
 
-import "fmt"
+import (
+	"fmt"
+
+	stack "algorithms/03_stack"
+)
 
 type graph struct {
 	maxVertex   int
@@ -31,7 +35,38 @@ func (g *graph) addVertex(label string) {
 func (g *graph) addEdge(start, end int) {
 	g.adjMat[start][end] = 1
 	g.adjMat[end][start] = 1
+}
 
+func (g *graph) dfs() {
+	s := stack.NewIntStack(g.maxVertex)
+	g.vertexList[0].wasVisited = true
+	g.displayVertex(0)
+	s.Push(0)
+
+	for !s.IsEmpty() {
+		a, _ := s.Peek()
+		v := g.getAdjUnvisitedVertex(int(a))
+		if v == -1 {
+			s.Pop()
+		} else {
+			g.vertexList[v].wasVisited = true
+			g.displayVertex(v)
+			s.Push(int64(v))
+		}
+	}
+
+	for i := 0; i < g.vertexCount; i++ {
+		g.vertexList[i].wasVisited = false
+	}
+}
+
+func (g *graph) getAdjUnvisitedVertex(v int) int {
+	for i := 0; i < g.vertexCount; i++ {
+		if g.adjMat[v][i] == 1 && !g.vertexList[i].wasVisited {
+			return i
+		}
+	}
+	return -1
 }
 
 func (g *graph) displayVertex(v int) {
---
+++ 17_graph/graph_test.go
@@ -0,0 +1,26 @@
+package _7_graph
+
+import "testing"
+
+func TestGraph(t *testing.T) {
+	g := newGraph(10)
+	g.addVertex("A") // 0
+	g.addVertex("B") // 1
+	g.addVertex("C") // 2
+	g.addVertex("D") // 3
+	g.addVertex("E") // 4
+	g.addVertex("F") // 5
+	g.addVertex("G") // 6
+	g.addVertex("H") // 7
+	g.addVertex("I") // 8
+	g.addEdge(0, 1)
+	g.addEdge(0, 2)
+	g.addEdge(0, 3)
+	g.addEdge(0, 4)
+	g.addEdge(1, 5)
+	g.addEdge(5, 7)
+	g.addEdge(3, 6)
+	g.addEdge(6, 8)
+
+	g.dfs()
+}
